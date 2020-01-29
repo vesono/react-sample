e05c53812d3c99115b78b042a8a334ab54ee30a1
vesono <rbrb-sry229@outlook.com>
2020-01-29 15:47:53 +0900

    add amplify module, amplify api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useReducer } from 'react';
+import API, { graphqlOperation } from '@aws-amplify/api';
+import PubSub from '@aws-amplify/pubsub';
+import awsconfig from './aws-exports';
 import './App.css';
 
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
+
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
+
+// 新しいタスクを作成
+async function createNewTodo() {
+  const todo = { name: "Use AWS AppSync" , description: "Realtime and Offline" };
+  // 新規タスク作成
+  await API.graphql(graphqlOperation(createTodo, { input: todo }));
+}
+
+const QUERY = 'QUERY';
+const initialState = {
+  todos: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case QUERY:
+      return {...state, todos: action.todos};
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function getData() {
+      const todoData = await API.graphql(graphqlOperation(listTodos));
+      dispatch({ type: QUERY, todos: todoData.data.listTodos.items });
+    }
+    getData();
+  }, []);
+
   return (
+    <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={createNewTodo}>Add Todo</button>
     </div>
+    <div>
+      {state.todos.length > 0 ? 
+        state.todos.map((todo) => <p key={todo.id}>{todo.name} : {todo.description}</p>) :
+        <p>Add some todos!</p> 
+      }
+    </div>
+  </div>
   );
 }
 
