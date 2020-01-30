b34f7b827020b813668af6fedbafc8cd920c02f7
vesono <rbrb-sry229@outlook.com>
2020-01-30 14:19:53 +0900

    add subscribe (tutorial)

--- src/App.js
+++ src/App.js
@@ -6,41 +6,58 @@ import './App.css';
 
 import { createTodo } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
+import { onCreateTodo } from './graphql/subscriptions';
 
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
 
-// 新しいタスクを作成
-async function createNewTodo() {
-  const todo = { name: "Use AWS AppSync" , description: "Realtime and Offline" };
-  // 新規タスク作成
-  await API.graphql(graphqlOperation(createTodo, { input: todo }));
-}
-
+// アクションタイプ
 const QUERY = 'QUERY';
+const SUBSCRIPTION = 'SUBSCRIPTION';
+
 const initialState = {
   todos: [],
 };
 
+// 新規タスク作成
+async function createNewTodo() {
+  const todo = { name: "Use AWS AppSync" , description: "Realtime and Offline" };
+  await API.graphql(graphqlOperation(createTodo, { input: todo }));
+}
+
+// reducer定義
 const reducer = (state, action) => {
   switch (action.type) {
     case QUERY:
       return {...state, todos: action.todos};
+      case SUBSCRIPTION:
+        console.log({...state, todos:[...state.todos, action.todo]});
+        return {...state, todos:[...state.todos, action.todo]};
     default:
       return state;
   }
 };
 
+// main処理
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
+    // タスク一覧を取得（初めに動く）
     async function getData() {
       const todoData = await API.graphql(graphqlOperation(listTodos));
       dispatch({ type: QUERY, todos: todoData.data.listTodos.items });
     }
     getData();
-  }, []);
+    // 変更を検知して、再取得を実施する
+    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+      next: (eventData) => {
+        const todo = eventData.value.data.onCreateTodo;
+        dispatch({ type: SUBSCRIPTION, todo });
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [])
 
   return (
     <div>
@@ -49,8 +66,12 @@ function App() {
     </div>
     <div>
       {state.todos.length > 0 ? 
-        state.todos.map((todo) => <p key={todo.id}>{todo.name} : {todo.description}</p>) :
-        <p>Add some todos!</p> 
+        state.todos.map(todo => (
+          <p key={todo.id}>
+            {todo.name} : {todo.description}
+          </p>
+        )) :
+          <p>Add some todos!</p> 
       }
     </div>
   </div>
